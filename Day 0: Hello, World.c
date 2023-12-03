42e70bd8cfde90e69de8bfe5c1b3a83aa4c0bc09
Pranav Das <138027582+Pranavdas-1@users.noreply.github.com>
2023-12-04 00:21:16 +0530

    Create Day 0: Hello, World.c

---
+++ Day 0: Hello, World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int c[100];
+gets(c);
+    printf("Hello, World.\n");
+    puts(c);
+    return 0;
+}
