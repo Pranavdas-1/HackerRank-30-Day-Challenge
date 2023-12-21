55004a129ffab04ecd7638b0db00e249c7578c99
Pranav Das <138027582+Pranavdas-1@users.noreply.github.com>
2023-12-21 18:55:12 +0530

    Create Day 18: Queues and Stacks.java

---
+++ Day 18: Queues and Stacks.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    LinkedList<Character> queue = new LinkedList();
+    LinkedList<Character> stack = new LinkedList();
+    
+    public void pushCharacter(char ch){
+        stack.push(ch);
+    }
+    
+    public void enqueueCharacter(char ch){
+        queue.add(ch);
+    }
+    
+    public char popCharacter(){
+        return stack.pop();
+    }
+    
+    public char dequeueCharacter(){
+        return queue.remove();
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Solution p = new Solution();
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;                
+                break;
+            }
+        }
+        System.out.println( "The word, " + input + ", is " 
+                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+}
